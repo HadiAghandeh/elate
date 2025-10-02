5178e7365b67972681c9950c1558dba18040c8e9
HadiAghandeh <hadi.aghandeh@gmail.com>
2025-10-02 15:26:20 +0330

    auth ui

--- app/dash/page.tsx
+++ app/dash/page.tsx
@@ -9,13 +9,15 @@ Amplify.configure(outputs);
 
 export default function App() {
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user?.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
-    </Authenticator>
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+        <Authenticator>
+        {({ signOut, user }) => (
+            <main>
+            <h1>Hello {user?.username}</h1>
+            <button onClick={signOut}>Sign out</button>
+            </main>
+        )}
+        </Authenticator>
+    </div>
   );
 }
